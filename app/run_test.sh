7c084e5aef2df543335a093c18c68c891dcd39bc
BOBO-LU <luwenkai0820@gmail.com>
2023-03-03 14:53:52 +0800

    modify github action
    change run
    
    change ci path
    
    change ci path
    
    change ci working dir
    
    change ci working dir
    
    change run script
    
    change path
    
    change path

--- app/run_test.sh
+++ app/run_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ENTRYPOINT="python -m pytest --cov=backend --html=test/report/report.html -W ignore::DeprecationWarning"
-docker build -t pytest-runner .
+docker build -f ./app/backend/Dockerfile -t pytest-runner .
 docker run --rm -it -v $(pwd)/app:/app pytest-runner $ENTRYPOINT
