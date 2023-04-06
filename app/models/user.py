353b756a77fb3f26c76076eaad22834e6da2e2b4
BOBO-LU <luwenkai0820@gmail.com>
2023-04-06 15:28:57 +0800

    move teammatch

---
+++ app/models/user.py
@@ -0,0 +1,22 @@
+import uuid
+from app.database.base_class import Base
+from sqlalchemy import DateTime, Column, String, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+
+
+class User(Base):
+    __tablename__ = "User"
+    user_uuid = Column(
+        UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4
+    )
+    name = Column(String, nullable=False, default="--")
+    password = Column(String, nullable=True)
+    email = Column(String, unique=True, nullable=False)
+    line_id = Column(String, unique=True, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    image = Column(String, nullable=True)
+    is_admin = Column(Boolean, nullable=False, default=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    is_google_sso = Column(Boolean, nullable=True, default=False)
