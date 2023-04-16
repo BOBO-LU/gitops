35b46cf171a96cd85918df9fd4a71a6aca2ec05a
BOBO-LU <luwenkai0820@gmail.com>
2023-04-16 17:51:12 +0800

    remove validator

--- app/core/config.py
+++ app/core/config.py
@@ -9,13 +9,13 @@ class Settings(BaseSettings):
     test_int: int = 50
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
+    # @validator("BACKEND_CORS_ORIGINS", pre=True)
+    # def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+    #     if isinstance(v, str) and not v.startswith("["):
+    #         return [i.strip() for i in v.split(",")]
+    #     elif isinstance(v, (list, str)):
+    #         return v
+    #     raise ValueError(v)
 
     # database
     DATABASE_PORT: int
